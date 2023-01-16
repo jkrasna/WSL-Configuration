0065611b8139f635509ef6487967e18f74e7a61a
Jure Krasna <jure.krasna@cosylab.com>
2023-01-16 10:34:56 +0100

    Added .tmux configuration files and scripts to run the custom session

---
+++ tmux_workenv_ede.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Session name (to avoid repeated occurences)
+S_NAME="Dev"
+
+# Name for first window name
+W_NAME="dockerd"
+
+# Command to execute in the first window
+W_CMD="./run_docker_daemon.sh"
+
+# Start the session and window 0 in the user home folder
+#   This will also be the default cwd for new windows created
+#   via a binding unless overridden with default-path.
+cd ~
+tmux new-session -s $S_NAME -n $W_NAME -d $W_CMD 
+
+# Create two windows in ~/workspace folder
+cd ~/workspace
+tmux new-window -a -n "Main"
+tmux new-window -a -n "Side"
+
+# Select window #2 (indexes start with 1)
+tmux select-window -t "$S_NAME:2"
+
+# Attach to the session
+tmux -2 attach-session -t "$S_NAME"
